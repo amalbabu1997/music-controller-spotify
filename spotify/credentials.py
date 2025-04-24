3aed8ae7ea40e49c3268bd6b7f44111c58f732b9
amalbabu1997 <124864200+amalbabu1997@users.noreply.github.com>
2025-04-24 18:04:28 -0400

    Update credentials.py again for assuring
    update

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,4 @@
 CLIENT_ID= "client ID from spotify developers webiste"
-CLIENT_SECRET="key for spotify developers webiste "
+CLIENT_SECRET="key for spotify developers webiste"
 REDIRECT_URI="http://127.0.0.1:8000/spotify/redirect"
 # https://developer.spotify.com/documentation/web-api/tutorials/getting-started
